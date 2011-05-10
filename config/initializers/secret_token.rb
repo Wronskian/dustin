f9b6660713888eb5b7f2d82a7e9a3e31bbae850c
Ryan Dalke <rdalke@gmail.com>
2011-05-10 14:48:43 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dustin::Application.config.secret_token = '6c0580f6523d6bdb590d1ae535a89519ce9a4cd25f45cd95c4ab2c66227bfc11125583beb756e75a014b0039c2a1615e7ac555f4fccd797cd78f8b42d897cb35'
